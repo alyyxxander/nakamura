7480ef8a9616b74fb3025993dc0746c7dd925487
alyyxxander <alyxs.second.acc@gmail.com>
2022-04-14 14:35:26 -0400

    added id's to sql items

--- takeout-panels/smallplatesPanel.php
+++ takeout-panels/smallplatesPanel.php
@@ -1,22 +1,22 @@
 <!-- small plates panel -->
 <div class="takeout-panel" id="smallplatesPanel">
 
-    <div id="ChickenGyoza" class="item" draggable="true" data-price="10.00">Chicken Gyoza (7 pieces) - $10</div>
+    <div id="chickenGyoza" class="item" draggable="true" data-price="10.00">Chicken Gyoza (7 pieces) - $10</div>
 
-    <div id="ChiliCabbage" class="item" draggable="true" data-price="6.00">Chili Cabbage - $6</div>
+    <div id="chiliCabbage" class="item" draggable="true" data-price="6.00">Chili Cabbage - $6</div>
 
-    <div id="SpicyYuzuWontonSoup" class="item" draggable="true" data-price="10.00">Spicy Yuzu Wonton Soup - $10</div>
+    <div id="spicyYuzuWontonSoup" class="item" draggable="true" data-price="10.00">Spicy Yuzu Wonton Soup - $10</div>
 
-    <div id="TeriyakiChashuP" class="item" draggable="true" data-price="9.00">Teriyaki Chashu (pork) - $9</div>
+    <div id="teriyakiChashuP" class="item" draggable="true" data-price="9.00">Teriyaki Chashu (pork) - $9</div>
 
-    <div id="TeriyakiChashuC" class="item" draggable="true" data-price="9.00">Teriyaki Chashu (chicken) - $9</div>
+    <div id="teriyakiChashuC" class="item" draggable="true" data-price="9.00">Teriyaki Chashu (chicken) - $9</div>
 
-    <div id="EdamamePesto" class="item" draggable="true" data-price="8.00">Edamame Pesto - $8</div>
+    <div id="edamamePesto" class="item" draggable="true" data-price="8.00">Edamame Pesto - $8</div>
 
-    <div id="MushroomCauliflower" class="item" draggable="true" data-price="10.00">Mushroom Cauliflower - $10</div>
+    <div id="mushroomCauliflower" class="item" draggable="true" data-price="10.00">Mushroom Cauliflower - $10</div>
 
-    <div id="YuzuWings" class="item" draggable="true" data-price="15.00">Yuzu Wings - $15</div>
+    <div id="yuzuWings" class="item" draggable="true" data-price="15.00">Yuzu Wings - $15</div>
 
-    <div id="ShishitoPeppers" class="item" draggable="true" data-price="8.00">Shishito Peppers - $8</div>  
+    <div id="shishitoPeppers" class="item" draggable="true" data-price="8.00">Shishito Peppers - $8</div>  
 
 </div>
